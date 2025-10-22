44a1dc5b7a932bf48c939a9174d36490d5803633
Felipe Correa Ramos <fcramos086@gmail.com>
2025-10-22 19:58:06 -0300

    feat(vuetify): replace tailwind by vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-
-  modules: [
-    '@nuxt/ui',
-    '@nuxt/image',
-    '@nuxt/eslint',
-  ]
-})
+  modules: ["@nuxt/eslint"],
+  css: ["vuetify/styles", "@mdi/font/css/materialdesignicons.css"],
+  build: {
+    transpile: ["vuetify"],
+  },
+  vite: {
+    define: {
+      "process.env.DEBUG": false,
+    },
+  },
+});
